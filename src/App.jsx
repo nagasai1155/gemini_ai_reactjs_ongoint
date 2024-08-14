e773566e4bec4d6acd16988493b6625c28063be3
nagasai1155 <nagasaibalam@gmail.com>
2024-08-14 21:29:44 +0530

    sidebar done with greate ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar.jsx';
+
+
+function App() {
+  return (
+   <>
+ 
+   
+   
+   <Sidebar></Sidebar>
+  
+   </>
+  )
+}
+
+export default App
