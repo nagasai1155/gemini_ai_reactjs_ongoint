567e3cb0ce24b690e11c58b511b1a3215334f16b
nagasai1155 <nagasaibalam@gmail.com>
2024-08-16 21:13:20 +0530

    complete ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 import Sidebar from './components/sidebar/Sidebar.jsx';
+import { Main } from './components/main/Main.jsx';
 
 
 function App() {
   return (
    <>
- 
+    <Sidebar></Sidebar>
+    <Main></Main>
    
-   
-   <Sidebar></Sidebar>
-  
    </>
+   
+   
+  
+  
+   
   )
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,8 +11,8 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="top">
-                <img src={assets.menu_icon} alt="" className="menu" onClick={() => setExtended(prev => !prev)} />
-                <div onClick={()=>newChat()} className="new-chat">
+                <img src={assets.menu_icon} alt="" className="menu" onClick={() => setExtended(!extended)} />
+                <div className="new-chat">
                     <img src={assets.plus_icon} alt="" />
                     {extended ? <p>New Chat</p> : null}
                 </div>
